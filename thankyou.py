0e5a83399a5165a29fdaea80161f0a4766fa5fa1
unknown <02230269.cst@rub.edu.bt>
2024-03-20 23:20:47 +0600

    first commit

---
+++ thankyou.py
@@ -0,0 +1,18 @@
+name = []
+grade = set()
+age{}
+name.append('pema wangdi')
+grade.add('1')
+age['19']='pema wangdi'
+name.append('kishor kumar ghalley')
+grade.add('2')
+age['21']
+name.append('tshering dorji')
+grade.add('1')
+age['20']
+searchname=input('enter students name')
+if searchname in name:
+    print(f'name found! grade:{age[searchname]}')
+else:
+    print:('name not found')
+    
